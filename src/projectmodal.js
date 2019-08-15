a8b84a44baeef89296f9a400ceae1664f65fe5cc
Angelica Hoyss <angelicahoyss@gmail.com>
2019-08-15 22:41:14 +0200

    styled post uploader and page

--- src/projectmodal.js
+++ src/projectmodal.js
@@ -32,9 +32,6 @@ export default class ProjectModal extends React.Component {
                 <div className="project-modal-info">
                     <p className="project-modal-info-title">{this.state.title}</p>
                     <p className="project-modal-info-description">{this.state.description}</p>
-                        <p>
-                            posted by {this.state.first} {this.state.last}
-                        </p>
                 </div>
             </div>
         );
